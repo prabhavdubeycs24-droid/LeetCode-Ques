5a7c9f546ef6dc54cf44cd74f25ee0ded427d0e6
Prabhav <prabhavdubey.cs24@bmsce.ac.in>
2026-09-01 00:28:17 +0530

    Time: 15 ms (90.86%), Space: 69.7 MB (79.95%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -7,38 +7,24 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
- // all r uniqueee
 class Solution {
-    public static boolean exist(TreeNode root , int val){
-        if(root==null){
-            return false;
-        }
-        if(root.val==val){
-            return true;
-        }
-        if(exist(root.left,val)==true || exist(root.right,val)==true){
-            return true;
-        }
-        return false;
-    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root==null){
             return null;
         }
         if(p.val==root.val || q.val==root.val){
-            return root;
+            return root ;
         }
-        // 3 case dono diff LST aur RST , dono LST , ya fhir dono RST 
-        if((exist(root.left,p.val) && exist(root.right,q.val))||(exist(root.left,q.val) && exist(root.right,p.val))){
-            return root;
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if(left!=null && right!=null){
+            return root ;
         }
-        else if(exist(root.left,p.val) && exist(root.left,q.val)){
-            return lowestCommonAncestor(root.left,p,q);
+        else if(left!=null){
+            return left;
         }
         else{
-            return lowestCommonAncestor(root.right,p,q);
+            return right;
         }
-
     }
-}//what if regardless of the constraint one of the node is not present in the tree, then what ans ?
-//"the present node will be the answer"
+}
