05f832b3866dac173442c826b8ddb4fbccb4ce15
Prabhav <prabhavdubey.cs24@bmsce.ac.in>
2026-09-15 16:31:03 +0530

    Time: 21 ms (58.01%), Space: 48.3 MB (11.05%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,25 +1,27 @@
+//scanning from left coz we want to make firstly make smaller the left side of the number(which hold highest place value)
 class Solution {
-    public String removeKdigits(String num, int k) {
+    public String removeKdigits(String s, int k) {
+        if(k==s.length()){
+            return "0";
+        }
         Stack<Character> st = new Stack<>();
-        int n = num.length();
-        int idx = 1;
-        st.push(num.charAt(0));
-        while(idx<n){
+        st.push(s.charAt(0));
+        int idx = 1 ;
+        while(idx<s.length()){
             if(st.size()==0){
-                st.push(num.charAt(idx));
+                st.push(s.charAt(idx));
                 idx++;
             }
-            else if(k>0 && num.charAt(idx)<st.peek()){
+            else if(k!=0 && Character.getNumericValue(s.charAt(idx))<Character.getNumericValue(st.peek())){
                 st.pop();
                 k--;
             }
             else{
-                st.push(num.charAt(idx));
+                st.push(s.charAt(idx));
                 idx++;
-                
             }
-        }  
-        while(k!=0 && st.size()!=0){
+        }
+        while(k!=0){
             st.pop();
             k--;
         }
@@ -27,12 +29,12 @@ class Solution {
         while(st.size()!=0){
             sb.append(st.pop());
         }
-        sb=sb.reverse();
-        while( sb.length()!= 0  && sb.charAt(0)=='0')
+        sb.reverse();
+        while(sb.length()!=0 && sb.charAt(0)=='0'){
             sb.deleteCharAt(0);
-            
-        if(sb.length()==0){
-            return "0" ;
+        }
+        if(sb.toString().equals("")){
+            return "0";
         }
         return sb.toString();
     }
