5408955f624f18c6f2125f62bdc27edf3dbdf7ec
Prabhav <prabhavdubey.cs24@bmsce.ac.in>
2026-09-17 15:11:32 +0530

    Time: 0 ms (100%), Space: 43.1 MB (15.77%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -16,7 +16,7 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null && q==null){
-            return true ;
+            return true;
         }
         if(p==null || q==null){
             return false;
@@ -24,12 +24,12 @@ class Solution {
         if(p.val!=q.val){
             return false;
         }
-        if(isSameTree(p.left,q.left)==false){
+        if(!isSameTree(p.left,q.left)){
             return false;
         }
-        if(isSameTree(p.right,q.right)==false){
+        if(!isSameTree(p.right,q.right)){
             return false;
         }
-        return true; 
+        return true ; 
     }
 }
