817d4200f90f55cda26f5fb6e5d04ba17cdc2013
Prabhav <prabhavdubey.cs24@bmsce.ac.in>
2026-09-21 10:24:52 +0530

    Time: 9 ms (67.96%), Space: 44.2 MB (14.8%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int n1 = s.length();
+        int n2 = t.length();
+        if(n1!=n2){
+            return false;
+        }
+        int[] freq1 = new int[256];
+        int[] freq2 = new int[256];
+        for(int i=0;i<n1;i++){
+            if(freq1[s.charAt(i)]!=0 && freq1[s.charAt(i)]!=t.charAt(i)){
+                return false ; 
+            }
+            freq1[s.charAt(i)]=t.charAt(i);
+        }
+        for(int j=0;j<n1;j++){
+            if(freq2[t.charAt(j)]!=0 && freq2[t.charAt(j)]!=s.charAt(j)){
+                return false ; 
+            }
+            freq2[t.charAt(j)]=s.charAt(j);
+        }
+        return true ; 
+        
+    }
+}
