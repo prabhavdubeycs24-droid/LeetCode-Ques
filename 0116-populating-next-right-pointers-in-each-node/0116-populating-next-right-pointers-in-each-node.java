9c64ae7ff95d1f0059295b3d7c054a10713232d7
Prabhav <prabhavdubey.cs24@bmsce.ac.in>
2026-09-23 23:49:57 +0530

    Time: 2 ms (52.33%), Space: 46.3 MB (86.88%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+// maintain a prev to connect temp ; 
+class Solution {
+    public Node connect(Node root) {
+        if(root==null){
+            return null ;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(q.size()!=0){
+            int lvl = q.size();
+            Node prev = null;
+            for(int i=0;i<lvl;i++){
+                Node temp = q.remove();
+                if(prev!=null){
+                    prev.next=temp;
+                }
+                prev=temp;
+                if(temp.left!=null){
+                    q.add(temp.left);
+                }
+                if(temp.right!=null){
+                    q.add(temp.right);
+                }
+            }
+        }
+        return root ; 
+    }
+}
