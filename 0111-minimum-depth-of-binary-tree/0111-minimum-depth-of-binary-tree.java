228d8766890eccf7dfdba2b1bd405ba78966e327
Prabhav <prabhavdubey.cs24@bmsce.ac.in>
2026-09-23 17:38:32 +0530

    Time: 5 ms (29.91%), Space: 82.2 MB (13.19%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -21,11 +21,11 @@ class Solution {
         if(root.left==null && root.right==null){
             return 1 ; 
         }
+        if(root.right==null){
+            return 1 + minDepth(root.left);
+        }
         if(root.left==null){
             return 1+minDepth(root.right);
-        }
-        if(root.right==null){
-            return 1+minDepth(root.left);
         }
         return 1+Math.min(minDepth(root.right),minDepth(root.left));
     }
