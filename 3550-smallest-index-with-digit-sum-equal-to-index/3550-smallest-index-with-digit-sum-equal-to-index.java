230c9c832bb3b92ddba88ea75440a26cbf5bb257
Prabhav <prabhavdubey.cs24@bmsce.ac.in>
2026-09-24 17:58:29 +0530

    Time: 1 ms (99.69%), Space: 45.6 MB (40.74%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -1,16 +1,18 @@
 class Solution {
     public int smallestIndex(int[] arr) {
         for(int i=0;i<arr.length;i++){
-            StringBuilder sb = new StringBuilder();
-            sb.append(arr[i]);
-            int sum = 0;
-            while(sb.length()!=0){
-                sum = sum + sb.charAt(0)-'0';
-                sb.deleteCharAt(0);
+            int t = arr[i];
+            int x = t%10;
+            t=t/10;
+            int y = t%10;
+            t=t/10;
+            int z= t%10;
+            t=t/10;
+            int z1 = t%10;
+            if(x+y+z+z1==i){
+                return i ;
             }
-            if(sum==i){
-                return i ; 
-            }
+            
         }
         return -1;
     }
