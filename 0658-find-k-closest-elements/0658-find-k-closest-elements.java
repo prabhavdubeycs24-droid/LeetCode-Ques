aa8c002c9e0235ebae0167702681aa94e3f91ebc
Prabhav <prabhavdubey.cs24@bmsce.ac.in>
2026-09-26 16:41:02 +0530

    Time: 4 ms (96.8%), Space: 47.7 MB (65.46%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,38 +1,23 @@
-public class pair implements Comparable<pair>{
-    int ele ;
-    int dist ;
-    pair(int ele , int dist){
-        this.ele=ele;
-        this.dist=dist;
-    }
-    public int compareTo(pair p){ // descending
-        if(p.dist==this.dist){
-            return Integer.compare(p.ele,this.ele);
-        }
-        return Integer.compare(p.dist,this.dist);
-    }
-
-}
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int n = arr.length;
-        PriorityQueue<pair> pq = new PriorityQueue<>();
-        for(int i=0;i<n;i++){
-            pq.add(new pair(arr[i],Math.abs(arr[i]-x)));
-        }
-        while(pq.size()>k){
-            pq.remove();
-        }
-        PriorityQueue<Integer> pq1 = new PriorityQueue<>();
-        while(pq.size()!=0){
-            pq1.add(pq.peek().ele);
-            pq.remove();
+        int i =0;
+        int j = k-1;
+        while(j<arr.length-1){
+            
+            if(Math.abs(arr[i]-x)>Math.abs(arr[j+1]-x)|| (Math.abs(arr[i]-x) == Math.abs(arr[j+1]-x)
+        && arr[i] == arr[j+1])){
+                i++;
+                j++;
+            }
+            else{
+                break;
+            }
         }
         ArrayList<Integer> al = new ArrayList<>();
-        while(pq1.size()!=0){
-            al.add(pq1.remove());
+        while(i<=j){
+            al.add(arr[i]);
+            i++;
         }
-        
-        return al ;
+        return al ; 
     }
 }
